02f788e08a4f4794a61508aebce80b4faa0699b0
nrdittbenner <36045957+nrdittbenner@users.noreply.github.com>
2018-06-27 14:18:11 -0500

    Add files via upload

---
+++ MatlabDataToFigure.m
@@ -0,0 +1,76 @@
+clearvars -except NBCe1BHAN635IRBIT626182
+Data = table2array(NBCe1BHAN635IRBIT626182);
+j = 1;
+n = 0;
+m = 1;
+w = 1;
+o = 1;
+y = 1;
+Mutation = input('Which mutation? ','s');
+
+for i=1:length(Data)
+timelist(j) = Data(i,1);
+Ilist(j) = Data(i,2);
+    if j == 10
+        n = n+1;
+        for k = 1:j
+            avgtime = sum(timelist)/length(timelist);
+            avgI = sum(Ilist)/length(Ilist);
+        end
+        time(n) = avgtime;
+        I(n) = avgI;
+    end
+j = j+1;
+if j > 10
+    j = 1;
+end
+end
+hold on
+plot(time,I*10^9,'b-')
+ylim([-2525 2525]);
+title(Mutation);
+xlabel('time(s)');
+ylabel('I(nA)');
+disp('Please make note of the times that you want the solution change labels at.');
+NumberChanges = input('How many solution changes? ');
+for q = 1:NumberChanges
+    disp(['Solution Change: ',num2str(q)]);
+    soln{q} = input('Name of solution: ','s');
+    tpoint(q) = input('Time of solution change? ');
+    
+end
+for p = 1:length(Data)
+    if Data(p,1) > tpoint(y)
+        Ipoint(y) = Data(p,2);
+        y = y+1;
+    end
+    if y > NumberChanges
+        break;
+    end
+end
+
+for l=1:NumberChanges
+    txt = soln{l};
+    solutionlabels = text(tpoint(l),(Ipoint(l)*10^9)-200,txt);
+    solutionlabels.FontWeight = 'bold';
+    solutionlabels.BackgroundColor = 'y';
+end
+option = input('Labels in the right place? (if no, close the figure) ','s');
+if isequal(option,'no')
+    for h = 1:NumberChanges
+        disp(['Label: ', num2str(h)]);
+        move = input('How many seconds over? ');
+        tpoint(h) = tpoint(h)+move;
+    end
+    plot(time,I*10^9,'b-')
+    ylim([-2525 2525]);
+    title(Mutation);
+    xlabel('time(s)');
+    ylabel('I(nA)');
+    for l=1:NumberChanges
+        txt = soln{l};
+        solutionlabels = text(tpoint(l),(Ipoint(l)*10^9)-200,txt);
+        solutionlabels.FontWeight = 'bold';
+        solutionlabels.BackgroundColor = 'y';
+    end
+end
---
+++ MatlabIVAnalysis.m
@@ -0,0 +1,65 @@
+%%Import IV's with titles ND96, Na, and CO2 as the variable names with
+%%respect to the solutions
+hold on
+titleIV = input('What is the title of your IV? ','s');
+for u = 1:3
+clearvars -except ND96 && Na && CO2 && u && titleIV && g
+if u == 1
+    IVData = table2array(ND96);
+    plot(IVData(:,1)*10^3,IVData(:,2)*10^6,'-g^')
+elseif u == 2
+    IVData = table2array(CO2);
+    plot(IVData(:,1)*10^3,IVData(:,2)*10^6,'-rp')
+elseif u == 3
+    IVData = table2array(Na);
+    plot(IVData(:,1)*10^3,IVData(:,2)*10^6,'-bs')
+end
+title(titleIV);
+xlabel('mV')
+ylabel('I(\mu A)')
+legend('ND96','5% CO2','0 Na')
+
+V1 = -0.02;
+V2 = -0.06;
+j = 1;
+n = 1;
+for i = 1:length(IVData)
+    if IVData(i,1) == V1
+        xval1(j) = IVData(i,1);
+        yval1(j) = IVData(i,2);
+        j = j+1;
+        V1 = V1+0.02;
+    end
+    if V1 > 0.02
+        break;
+    end
+end
+for m = 1:length(IVData)
+    if IVData(m,1) >= V2 || IVData(m,1) > V2-0.001
+        xval2(n) = IVData(m,1);
+        yval2(n) = IVData(m,2);
+        n = n+1;
+        V2 = V2+0.02;
+    end
+    if V2 > 0.001
+        break;
+    end
+end
+
+if u == 1
+    disp('ND96')
+elseif u ==2
+    disp('5% CO2')
+elseif u ==3
+    disp('0 Na')
+end
+
+p1 = polyfit(xval1,yval1,1);
+p2 = polyfit(xval2,yval2,1);
+g(u,:) = [p1(1) p2(1)];
+
+disp(['Conductance -20mV-20mV (Siemens): ' num2str(p1(1))])
+disp(['Conductance -60mV-0mV (Siemens): ' num2str(p2(1))])
+
+ 
+end
