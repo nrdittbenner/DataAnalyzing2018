80f32196c287bc321c55c9ad5564b38b1ce527fa
nrdittbenner <36045957+nrdittbenner@users.noreply.github.com>
2018-07-04 22:21:48 -0500

    Add files via upload

--- MatlabDataToFigure.m
+++ MatlabDataToFigure.m
@@ -1,11 +1,7 @@
-clearvars -except NBCe1BHAN635IRBIT626182
-Data = table2array(NBCe1BHAN635IRBIT626182);
+clearvars -except NBCe1BHAN63SIRBIT62518
+Data = table2array(NBCe1BHAN63SIRBIT62518);
 j = 1;
 n = 0;
-m = 1;
-w = 1;
-o = 1;
-y = 1;
 Mutation = input('Title of your figure: ','s');
 
 for i=1:length(Data)
@@ -32,45 +28,16 @@ title(Mutation);
 xlabel('time(s)');
 ylabel('I(nA)');
 disp('Please make note of the times that you want the solution change labels at.');
-NumberChanges = input('How many solution changes? ');
-for q = 1:NumberChanges
-    disp(['Solution Change: ',num2str(q)]);
-    soln{q} = input('Name of solution: ','s');
-    tpoint(q) = input('Time of solution change? ');
-    
-end
-for p = 1:length(Data)
-    if Data(p,1) > tpoint(y)
-        Ipoint(y) = Data(p,2);
-        y = y+1;
-    end
-    if y > NumberChanges
-        break;
-    end
-end
-
-for l=1:NumberChanges
-    txt = soln{l};
-    solutionlabels = text(tpoint(l),(Ipoint(l)*10^9)-200,txt);
-    solutionlabels.FontWeight = 'bold';
-    solutionlabels.BackgroundColor = 'y';
-end
-option = input('Labels in the right place? (if no, close the figure) ','s');
-if isequal(option,'no')
-    for h = 1:NumberChanges
-        disp(['Label: ', num2str(h)]);
-        move = input('How many seconds over? ');
-        tpoint(h) = tpoint(h)+move;
-    end
-    plot(time,I*10^9,'b-')
-    ylim([-2525 2525]);
-    title(Mutation);
-    xlabel('time(s)');
-    ylabel('I(nA)');
-    for l=1:NumberChanges
-        txt = soln{l};
-        solutionlabels = text(tpoint(l),(Ipoint(l)*10^9)-200,txt);
-        solutionlabels.FontWeight = 'bold';
-        solutionlabels.BackgroundColor = 'y';
-    end
-end
+CO2Range = input('Input time 5% CO2 was added and time it was taken away ([t1 t2]): ');
+NaRange = input('Input time Na was taken away and time it was returned: ');
+co2area = fill([NaRange(1),NaRange(2),NaRange(2),NaRange(1)],[-2500 -2500 1500 1500],'yellow');
+co2area.FaceAlpha = 0.3;
+co2area.EdgeColor = 'none';
+naline = plot([NaRange(1) NaRange(2)], [1500 1500],'k-');
+naline.LineWidth = 1;
+c02line = plot([CO2Range(1) CO2Range(2)],[2000 2000],'k-','Marker','.');
+c02line.LineWidth = 1;
+co2label = text(((CO2Range(2)-CO2Range(1))/2)+CO2Range(1),2100,'5% CO2/33 mM HCO3/ND96','HorizontalAlignment','center');
+co2label.FontWeight = 'bold';
+nalabel = text(((NaRange(2)-NaRange(1))/2)+NaRange(1),1600,'0 Na 5% CO2/33 mM HCO3/ND96','HorizontalAlignment','center');
+nalabel.FontWeight = 'bold';
